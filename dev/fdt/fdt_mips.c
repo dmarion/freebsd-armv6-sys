9a438d494737412918f20a13f2ccc6ce71851fbc
cognet <cognet@ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f>
2011-10-25 19:47:28 +0000

    Attempt to MFC
    git-svn-id: svn://svn.freebsd.org/base/projects/armv6/sys@226747 ccf9f872-aa2e-dd11-9fc8-001c23d0bc1f

--- dev/fdt/fdt_mips.c
+++ dev/fdt/fdt_mips.c
@@ -1,6 +1,9 @@
 /*-
- * Copyright (c) 2001-2003, Shunsuke Akiyama <akiyama@FreeBSD.org>.
+ * Copyright (c) 2009-2010 The FreeBSD Foundation
  * All rights reserved.
+ *
+ * This software was developed by Semihalf under sponsorship from
+ * the FreeBSD Foundation.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
@@ -14,7 +17,7 @@
  * THIS SOFTWARE IS PROVIDED BY THE AUTHOR AND CONTRIBUTORS ``AS IS'' AND
  * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
  * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
- * ARE DISCLAIMED.  IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
+ * ARE DISCLAIMED. IN NO EVENT SHALL THE AUTHOR OR CONTRIBUTORS BE LIABLE
  * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
  * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS
  * OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
@@ -22,17 +25,32 @@
  * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY
  * OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF
  * SUCH DAMAGE.
- *
- * $FreeBSD$
  */
 
-#ifndef _RUEPHYREG_H_
-#define _RUEPHYREG_H_
+#include <sys/cdefs.h>
+__FBSDID("$FreeBSD$");
 
-#define RUEPHY_MII_MSR		0x0137	/* B, R/W */
-#define RUEPHY_MSR_RXFCE	0x40
-#define RUEPHY_MSR_DUPLEX	0x10
-#define RUEPHY_MSR_SPEED100	0x08
-#define RUEPHY_MSR_LINK		0x04
+#include <sys/param.h>
+#include <sys/systm.h>
+#include <sys/kernel.h>
+#include <sys/module.h>
+#include <sys/bus.h>
 
-#endif	/* _RUEPHYREG_H_ */
+#include <machine/intr_machdep.h>
+
+#include <dev/ofw/ofw_bus.h>
+#include <dev/ofw/ofw_bus_subr.h>
+#include <dev/ofw/openfirm.h>
+
+#include "ofw_bus_if.h"
+#include "fdt_common.h"
+
+struct fdt_fixup_entry fdt_fixup_table[] = {
+	{ NULL, NULL }
+};
+
+fdt_pic_decode_t fdt_pic_table[] = {
+	NULL,
+	NULL,
+	NULL
+};
